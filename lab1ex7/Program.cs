04f01f358efdd0324afe0db12e96c5845432a4a7
Andrei Corovei <andrei.corovei@qubiz.com>
2023-08-19 12:39:33 +0300

    Add project files.

---
+++ lab1ex7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lab1ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**Ex4
+            Scrieti un program care va verifica daca un numar citit de la tastatura este par sau impar
+           In cazul in care numarul este par, programul va afisa “par” iar in caz contrar, “impar”. */
+
+            int number;
+
+            Console.WriteLine("Enter any number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The number is even");
+            }
+            else if (number % 2 != 0)
+            {
+                Console.WriteLine("The number is odd");
+            }
+
+        }
+    }
+}
